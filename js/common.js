e841bf62081589bf50a730a118651846b0d53f9e
xy <598491063@qq.com>
2017-09-23 16:31:26 +0800

    2017-9-23

--- js/common.js
+++ js/common.js
@@ -2,12 +2,27 @@ var fc = {};
 //fc.post = 'http://location.zhijiasoft.com/';
 //高德地图web服务请求域名   web服务key: e00e7ed74876f674fa2f01bf36f429c8
 fc.post = 'http://restapi.amap.com/';
+//调试 post
+fc.urlPost = 'http://172.10.10.130:8000/';
 //jQuery ajax
 fc.ajax = function(url, param, type) {
 	var url = fc.post + url;
 	return $.ajax(url,param, type).then(function(data) {
 		console.log(data.infocode);
 		if(data.results || data.infocode ==  '10000') {
+			return data;
+		} else {
+			mui.toast('服务错误');
+		}
+	}, function(err) {
+		mui.toast('连接失败');
+	})
+}
+fc.commonAjax = function(url, param, type) {
+	var url = fc.urlPost + url;
+	return $.ajax(url,param, type).then(function(data) {
+		console.log(data.infocode);
+		if(data.results) {
 			return data;
 		} else {
 			mui.toast('服务错误');
--- js/test.js
+++
@@ -1,3 +0,0 @@
-function testFn() {
-	alert('this is testFn test');
-}
